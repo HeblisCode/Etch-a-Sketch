edf5c44d1ff08f56f1611263d68ad0c9bbe95265
Davide Stefania <davide16797@gmail.com>
2021-04-26 18:53:57 +0200

    all color modes working, mouseover handler reworked into draw()

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
-function createGrid(size) {
+/*CELL CREATION******************************************************************************************/
+function createCells(size) {
   const gameContainer = document.querySelector("#gameContainer");
   const cellSize = 600 / size;
   gameContainer.style.cssText = `
@@ -9,59 +10,88 @@ function createGrid(size) {
   for (let i = 0; i < size ** 2; i++) {
     const cell = document.createElement("div");
     cell.classList.add("gridCell");
+    cell.style.cssText = "background-color: rgb(255,255,255);";
     gameContainer.appendChild(cell);
   }
 }
 
+/*COLOR MODES*******************************************************************************************/
 function colorBlack(e) {
-  e.target.style.cssText = "background-color: hsla(0, 0%, 0%, 1);";
+  e.target.style.cssText = "background-color: hsl(0, 0%, 0%);";
 }
 function colorRainbow(e) {
   let randomHue = Math.floor(Math.random() * 360);
   e.target.style.cssText = `background-color: hsla(${randomHue}, 100%, 50%, 1)`;
 }
-function colorGradient(e) {
-  //WIP
-  console.log(e.target.style.backgroundColor);
+function colorDarken(e) {
+  e.target.style.backgroundColor = decreaseLightness(
+    e.target.style.backgroundColor
+  );
+}
+function colorLighten(e) {
+  e.target.style.backgroundColor = increaseLightness(
+    e.target.style.backgroundColor
+  );
 }
 
-function changeMode(mode) {
+/*MOUSEOVER HANDLER*******************************************************************************************/
+function draw(e, color) {
+  switch (color) {
+    case "black":
+      colorBlack(e);
+      break;
+    case "rainbow":
+      colorRainbow(e);
+      break;
+    case "darken":
+      colorDarken(e);
+      break;
+    case "lighten":
+      colorLighten(e);
+      break;
+    default:
+      colorBlack(e);
+      break;
+  }
+}
+
+/*NEW GAME - RESIZE*********************************************************************************************/
+function newGrid() {
+  let color; //stores the currently active color mode
+
+  if (arguments.length === 0) {
+    //page load
+    createCells(30);
+  } else {
+    //grid resize
+    const gameContainer = document.querySelector("#gameContainer");
+    gameContainer.innerHTML = "";
+    createCells(arguments[0].target.value);
+  }
+
+  //mouseover event listeners
   const cells = document.querySelectorAll(".gridCell");
-  const mouseoverListener = function (e) {
-    mode(e);
-  };
   cells.forEach((cell) =>
-    cell.removeEventListener("mouseover", mouseoverListener)
+    cell.addEventListener("mouseover", function (e) {
+      draw(e, color);
+    })
   );
-  cells.forEach((cell) =>
-    cell.addEventListener("mouseover", mouseoverListener)
+
+  //buttons event listeners
+  const colorModesArray = ["black", "rainbow", "lighten", "darken"];
+  let buttons = document.querySelectorAll("#buttonContainer > button");
+  buttons.forEach((button, i) =>
+    button.addEventListener("click", function () {
+      color = colorModesArray[i];
+    })
   );
 }
 
-function resizeGrid(e) {
-  const gameContainer = document.querySelector("#gameContainer");
-  gameContainer.innerHTML = "";
-  createGrid(e.target.value);
-  changeMode(colorBlack);
-}
-
+/*ON LOAD*******************************************************************************************/
 window.onload = function () {
-  createGrid(30);
-  changeMode(colorBlack); //default selection = black
-
-  const rainbowButton = document.querySelector("#rainbow");
-  const blackButton = document.querySelector("#black");
-  const gradientButton = document.querySelector("#gradient");
+  newGrid();
   const slider = document.querySelector("#slider");
-
-  rainbowButton.addEventListener("click", function () {
-    changeMode(colorRainbow);
+  slider.addEventListener("change", function (e) {
+    resizeGrid(e);
   });
-  blackButton.addEventListener("click", function () {
-    changeMode(colorBlack);
-  });
-  gradientButton.addEventListener("click", function () {
-    changeMode(colorGradient);
-  });
-  slider.addEventListener("change", resizeGrid);
 };
---
+++ lib.js
@@ -0,0 +1,70 @@
+//returns an array with the r, g, b values
+function getColorValues(string) {
+  //string = rgb(xxx, xxx, xxx)
+  let colorValuesString = string.slice(
+    string.indexOf("(") + 1,
+    string.length - 1
+  );
+  return colorValuesString.split(", ");
+}
+
+//accept an rgb array (both string or number) and returns a number array with [hue, saturation%, lightness%]
+function RGBtoHSL(rgbArray) {
+  //string = rgb(xxx, xxx, xxx);
+  let red = rgbArray[0] / 255;
+  let green = rgbArray[1] / 255;
+  let blue = rgbArray[2] / 255;
+  let maxColor = Math.max(red, green, blue);
+  let minColor = Math.min(red, green, blue);
+  let delta = maxColor - minColor;
+
+  let hue;
+  let saturation;
+  let lightness = (maxColor + minColor) / 2;
+
+  //calculate hue
+  if (delta === 0) {
+    hue = 0;
+  } else if (maxColor === red) {
+    hue = ((green - blue) / delta) % 6;
+  } else if (maxColor === green) {
+    hue = (blue - red) / delta + 2;
+  } else if (maxColor === blue) {
+    hue = (red - green) / delta + 4;
+  }
+  hue = Math.round(hue * 60);
+
+  //calculate saturation
+  if (delta === 0) {
+    saturation = 0;
+  } else {
+    saturation = delta / (1 - Math.abs(2 * lightness - 1));
+  }
+
+  return [hue, saturation * 100, lightness * 100];
+}
+
+//takes a string input of "rgb(xxx, xxx, xxx) and returns a hsl string with 10% more brightness"
+function increaseLightness(colorString) {
+  const RGBArray = getColorValues(colorString);
+  let HSLArray = RGBtoHSL(RGBArray);
+  if (HSLArray[2] === 100) {
+    return;
+  } else {
+    return `hsl(${HSLArray[0]}, ${HSLArray[1]}%, ${HSLArray[2] + 10}%)`;
+  }
+}
+
+//takes a string input of "rgb(xxx, xxx, xxx) and returns a hsl string with 10% less brightness"
+function decreaseLightness(colorString) {
+  const RGBArray = getColorValues(colorString);
+  let HSLArray = RGBtoHSL(RGBArray);
+
+  if (HSLArray[2] === 0) {
+    console.log("done");
+    return;
+  } else {
+    console.log(`darkening ${HSLArray}`);
+    return `hsl(${HSLArray[0]}, ${HSLArray[1]}%, ${HSLArray[2] - 10}%)`;
+  }
+}
