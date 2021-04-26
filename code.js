0e6c4165187b6790f975f1f0be142d78afb04375
Davide Stefania <davide16797@gmail.com>
2021-04-26 10:11:31 +0200

    first working prototype

--- code.js
+++ code.js
@@ -1,16 +1,21 @@
 function createGrid(size) {
   const gameContainer = document.querySelector("#gameContainer");
-  const cellSize = 800 / size;
+  const cellSize = 600 / size;
 
   for (let i = 0; i < size ** 2; i++) {
     const cell = document.createElement("div");
     cell.classList.add("gridCell");
-    cell.innerText = i;
-    cell.style.height = cellSize + "px";
-    cell.style.width = cellSize + "px";
-    console.log(cellSize);
+    cell.style.cssText = `height: ${cellSize}px; width: ${cellSize}px`;
     gameContainer.appendChild(cell);
   }
 }
 
-createGrid(10);
+function changeColor(e) {
+  e.target.classList.add("black");
+}
+
+window.onload = function () {
+  createGrid(10);
+  const cells = document.querySelectorAll(".gridCell");
+  cells.forEach((cell) => cell.addEventListener("mouseover", changeColor));
+};
